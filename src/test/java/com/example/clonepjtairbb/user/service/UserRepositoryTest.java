2ced0170a7f02ad68ad3c669beebde345dbd1333
DESKTOP-8BAC7IL\pak92 <pak926@nate.com>
2023-05-16 19:09:42 +0900

    feat : 필터 테스트 코드 작성

--- src/test/java/com/example/clonepjtairbb/user/service/UserRepositoryTest.java
+++ src/test/java/com/example/clonepjtairbb/user/service/UserRepositoryTest.java
@@ -2,13 +2,13 @@ package com.example.clonepjtairbb.user.service;
 
 import java.util.List;
 
-import org.aspectj.lang.annotation.After;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.test.context.TestPropertySource;
 
 import com.example.clonepjtairbb.user.entity.User;
