d19ad2264c3f4fbd6a1e716be6f6c57f67c65f09
DESKTOP-8BAC7IL\pak92 <pak926@nate.com>
2023-05-17 06:45:14 +0900

    feat : queryDSL 코드 수정

--- src/test/java/com/example/clonepjtairbb/stay/repository/QueryDSL/StayRepositoryCustomTest.java
+++ src/test/java/com/example/clonepjtairbb/stay/repository/QueryDSL/StayRepositoryCustomTest.java
@@ -1,78 +1,77 @@
-//package com.example.clonepjtairbb.stay.repository.QueryDSL;
-//
-//import static com.example.clonepjtairbb.stay.entity.QStay.*;
-//import static com.example.clonepjtairbb.stay.entity.QStayDetailFeature.*;
-//
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//
-//import org.springframework.context.annotation.Import;
-//import org.springframework.test.context.TestPropertySource;
-//
-//import com.example.clonepjtairbb.TestConfig;
-//import com.example.clonepjtairbb.common.enums.BedTypeEnum;
-//import com.example.clonepjtairbb.common.enums.CityEnum;
-//import com.example.clonepjtairbb.common.enums.CountryEnum;
-//import com.example.clonepjtairbb.common.enums.DescTagEnum;
-//import com.example.clonepjtairbb.common.enums.StayTypeEnum;
-//import com.example.clonepjtairbb.stay.dto.SearchOptionRequest;
-//import com.example.clonepjtairbb.stay.entity.QStay;
-//import com.example.clonepjtairbb.stay.entity.Stay;
-//import com.querydsl.core.types.dsl.BooleanExpression;
-//import com.querydsl.jpa.impl.JPAQueryFactory;
-//import static com.example.clonepjtairbb.stay.entity.QStay.stay;
-//import jakarta.persistence.EntityManager;
-//
-//
-//@TestPropertySource(locations = "/application.properties")
-//@DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//@Import({TestConfig.class})
-//class StayRepositoryCustomTest {
-//
-//	@Autowired
-//	private EntityManager entityManager;
-//
-//	@Autowired
-//	private StayRepositoryCustom testRepository;
-//
-//	@Test
-//	void findBySearchOption() {
-//		//given
-//		Integer cost = null;           // 숙박비
-//		String country = "";             // 나라
-//		String city = "";                   // 도시
-//		String stayType = "";           // 숙박 타입(주택, 캠핑카, 아파트 ..)
-//		Integer numBed = null;             // 침대갯수
-//		String bedType = "";             // 침대 타입
-//		String descTag = "";             // 숙박 시설 태그
-//		Integer maxGroupNum = null;   // 최대 게스트 수
-//
-//		Map<String, Object> data = new HashMap<>();
-//		data.put("costPerDay", cost);
-//		data.put("country", country);
-//		data.put("city", city);
-//		data.put("stayType", stayType);
-//		data.put("numBed", numBed);
-//		data.put("bedType", bedType);
-//		data.put("descTag", descTag);
-//		data.put("maxGroupNum", maxGroupNum);
-//
-//		System.out.println("테스트");
-//		SearchOptionRequest request = new SearchOptionRequest(data);
-//
-//		List<Stay> result = testRepository.findBySearchOption(request);
-//
-//		for (Stay stay : result) {
-//			System.out.println("stay.getTitle() = " + stay.getTitle());
-//			System.out.println("stay.toString() = " + stay.getContent());
-//		}
-//
-//	}
-//}
+package com.example.clonepjtairbb.stay.repository.QueryDSL;
+
+import static com.example.clonepjtairbb.stay.entity.QStay.*;
+import static com.example.clonepjtairbb.stay.entity.QStayDetailFeature.*;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+
+import com.example.clonepjtairbb.TestConfig;
+import com.example.clonepjtairbb.common.enums.BedTypeEnum;
+import com.example.clonepjtairbb.common.enums.CityEnum;
+import com.example.clonepjtairbb.common.enums.ConvenienceEnum;
+import com.example.clonepjtairbb.common.enums.CountryEnum;
+import com.example.clonepjtairbb.common.enums.DescTagEnum;
+import com.example.clonepjtairbb.common.enums.StayTypeEnum;
+import com.example.clonepjtairbb.stay.dto.MappedSearchRequest;
+import com.example.clonepjtairbb.stay.dto.SearchOptionRequest;
+import com.example.clonepjtairbb.stay.entity.QStay;
+import com.example.clonepjtairbb.stay.entity.Stay;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import static com.example.clonepjtairbb.stay.entity.QStay.stay;
+import jakarta.persistence.EntityManager;
+
+
+@TestPropertySource(locations = "/application.properties")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import({TestConfig.class})
+class StayRepositoryCustomTest {
+
+	@Autowired
+	private EntityManager entityManager;
+
+	@Autowired
+	private StayRepositoryCustom testRepository;
+
+	@Test
+	void findBySearchOption() {
+		//given
+		System.out.println("테스트를 위해 필터 조건을 입력해주세요");
+		MappedSearchRequest searchRequest = MappedSearchRequest.builder()
+			.country(CountryEnum.KOREA)
+			.city(CityEnum.SEOUL)
+			.cost_min(100)
+			.cost_max(500)
+			.stayType(StayTypeEnum.ROOM)
+			.numBed(2)
+			.bedType(BedTypeEnum.DOUBLE)
+			.isShared(false)
+			.descTag(DescTagEnum.ATTRACTIVE)     // Set the description tag
+			.checkin_date(Calendar.getInstance())
+			.checkout_date(Calendar.getInstance())
+			.groupsize(4)
+			.convenience(Arrays.asList(ConvenienceEnum.WIFI, ConvenienceEnum.WIFI))  // Set the list of conveniences
+			.build();
+		
+		List<Stay> result = testRepository.findBySearchOption(searchRequest);
+
+		for (Stay stay : result) {
+			System.out.println("stay.getTitle() = " + stay.getTitle());
+			System.out.println("stay.toString() = " + stay.getContent());
+		}
+
+	}
+}
